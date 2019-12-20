b356ef39e0fb7b947daae38cb028ad9c9c58339b
Patrick Wendel <ptrckwndl13@gmail.com>
2019-12-19 21:38:43 -0700

    worked on search bar

--- feeder/ui/calendar/src/App.js
+++ feeder/ui/calendar/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import AppBar from './components/js/Appbar'
+import SearchBar from './components/js/SearchBar'
 
 
 
@@ -9,9 +10,13 @@ function App() {
   return (
     <div className="App">
       <header>
-       <AppBar></AppBar>
+        <AppBar></AppBar>
       </header>
-      <div>Calendar</div>
+      <div>
+        <SearchBar></SearchBar>
+        <div>Calendar</div>
+      </div>
+
     </div>
   );
 }
---
+++ feeder/ui/calendar/src/components/js/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+function SearchBar() {
+
+    return (
+        <form>
+            Search
+            <input type="text" name="Search"></input>
+        </form>
+    )
+}
+
+export default SearchBar
