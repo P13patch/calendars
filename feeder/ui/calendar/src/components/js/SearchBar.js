9a5df3424b8ab705dd5debef590361e164b7f7c2
Patrick Wendel <ptrckwndl13@gmail.com>
2019-12-31 13:51:12 -0700

    worked on search filters

--- feeder/ui/calendar/src/components/js/SearchBar.js
+++ feeder/ui/calendar/src/components/js/SearchBar.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 
 function SearchBar() {
+    const [openField, setOpenField] = useState(false)
+
+
+    const searchClick = event => {
+        setOpenField(true)
+        if (openField === true) {
+           }
+    }
+
+    function searchField(openfield){
+        if (openfield === true) {
+
+        
+        return(
+            <div>
+                <ul>
+                    <li>filter</li>
+                    <li>filter</li>
+                </ul>
+            </div>
+        )
+        }
+        else {
+            return(
+                <div></div>
+            )
+        }
+    }
 
     return (
         <form>
             Search
-            <input type="text" name="Search"></input>
+            <input type="text" name="Search" onClick={searchClick}></input>
+            <div>
+               {searchField(openField)}
+            </div>
         </form>
     )
 }
